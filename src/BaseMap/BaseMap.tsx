6ded628e8e022b0f400b58f63bb94001286ba102
Lukas Winkler <git@lw1.at>
2024-04-03 23:08:22 +0200

    restore custom background map

--- src/BaseMap/BaseMap.tsx
+++ src/BaseMap/BaseMap.tsx
@@ -3,7 +3,7 @@ import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import * as pmtiles from 'pmtiles'
 import { useEffect, useState } from 'react'
-import type { LngLatBoundsLike } from 'react-map-gl/maplibre'
+import {Layer, LngLatBoundsLike, Source} from 'react-map-gl/maplibre'
 import { Map, type ViewStateChangeEvent } from 'react-map-gl/maplibre'
 import {
   $clickedMapData,
@@ -64,6 +64,7 @@ export const BaseMap = ({ initialViewState, interactiveLayerIds, boxZoom, childr
     setParamsMap(initialViewState)
   }, [])
 
+  const rasterAttribution='Data: © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>, <a href="http://viewfinderpanoramas.org/">SRTM</a>, <a href="https://portal.opentopography.org/datasetMetadata?otCollectionID=OT.032021.4326.2">NASADEM</a>, <a href="https://worldcover2021.esa.int">ESA WorldCover</a>; Maps © <a href="https://www.tracestrack.com/">Tracestrack</a>'
   const latLngZoom = baseMapSearchparamsParse(params.map)
 
   return (
@@ -75,7 +76,7 @@ export const BaseMap = ({ initialViewState, interactiveLayerIds, boxZoom, childr
             longitude: latLngZoom.longitude || initialViewState.longitude,
           }}
           // Style: https://cloud.maptiler.com/maps/dataviz/
-          mapStyle="https://api.maptiler.com/maps/dataviz/style.json?key=ur6Yh3ULc6QjatOYBgln"
+          // mapStyle="https://api.maptiler.com/maps/dataviz/style.json?key=ur6Yh3ULc6QjatOYBgln"
           style={{ width: '100%', height: '100%' }}
           boxZoom={boxZoom || true}
           // hash
@@ -90,6 +91,15 @@ export const BaseMap = ({ initialViewState, interactiveLayerIds, boxZoom, childr
           onMouseLeave={() => setCursorStyle('grab')}
           onClick={(event) => $clickedMapData.set(event.features)}
       >
+        <Source
+            type="raster"
+            tiles={['https://maps.lw1.at/tiles/1.0.0/tracestack/webmercator_hq/{z}/{x}/{y}.png']}
+            tileSize={512}
+            attribution={rasterAttribution}
+        >
+          <Layer type="raster"></Layer>
+
+        </Source>
         {children}
       </Map>
   )
